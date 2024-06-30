2a300907ec8b15ace0a534ccc9c201a0db7f90d1
JMart25 <149201983+JMart25@users.noreply.github.com>
2024-06-30 00:09:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,76 @@
+using System;
+
+namespace GreenvilleRevenue
+{
+    class Program
+    {
+        const int REVENUE_PER_CONTESTANT = 25;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                DisplayMotto();
+                DisplayMenu();
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    CalculateRevenue();
+                }
+                else if (choice == "2")
+                {
+                    Console.WriteLine("Thank you for using the Greenville Revenue App, good-bye!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+            }
+        }
+
+        static void DisplayMotto()
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("*  The stars shine in Greenville.  *");
+            Console.WriteLine("************************************");
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("\nPlease Enter the following number below from the following menu:");
+            Console.WriteLine("1. CALCULATE Greenville Revenue Year-Over-Year");
+            Console.WriteLine("2. Exit");
+        }
+
+        static void CalculateRevenue()
+        {
+            Console.Write("Enter number of contestants last year: ");
+            int lastYearContestants = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter number of contestants this year: ");
+            int thisYearContestants = int.Parse(Console.ReadLine());
+
+            int thisYearRevenue = thisYearContestants * REVENUE_PER_CONTESTANT;
+
+            Console.WriteLine($"\nLast year's competition had {lastYearContestants} contestants, and this year's has {thisYearContestants} contestants");
+            Console.WriteLine($"Revenue expected this year is ${thisYearRevenue}");
+
+            if (thisYearContestants > 2 * lastYearContestants)
+            {
+                Console.WriteLine("The competition is more than twice as big this year!");
+            }
+            else if (thisYearContestants > lastYearContestants)
+            {
+                Console.WriteLine("The competition is bigger than ever!");
+            }
+            else
+            {
+                Console.WriteLine("A tighter race this year! Come out and cast your vote!");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
